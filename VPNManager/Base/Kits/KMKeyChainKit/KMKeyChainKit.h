df8b868ee9789a07af3f0b71a9cc74d7e4ef15ee
Klein Mioke <565408238@qq.com>
2016-01-25 21:33:46 +0800

    update

---
+++ VPNManager/Base/Kits/KMKeyChainKit/KMKeyChainKit.h
@@ -0,0 +1,13 @@
+//
+//  KMKeyChainKit.h
+//  VPNManager
+//
+//  Created by Mioke on 1/25/16.
+//  Copyright © 2016 Mioke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMKeyChainKit : NSObject
+
+@end
---
+++ VPNManager/Base/Kits/KMKeyChainKit/KMKeyChainKit.m
@@ -0,0 +1,13 @@
+//
+//  KMKeyChainKit.m
+//  VPNManager
+//
+//  Created by Mioke on 1/25/16.
+//  Copyright © 2016 Mioke. All rights reserved.
+//
+
+#import "KMKeyChainKit.h"
+
+@implementation KMKeyChainKit
+
+@end
